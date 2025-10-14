699686b987632aa799ee566e6c15dcae60d31d7b
Javohir-lab12 <karimovjavohir0612@gmail.com>
2025-10-14 12:52:27 +0500

    week 3 exercise

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,3 @@
-temperature_in_celcius = float(input("What is temperature right now in celcius? "))
-temperature_in_faranheit = (temperature_in_celcius * 9/5)+32
-temperature_in_kelvin = temperature_in_celcius + 273.15
-print(f"Right now, the temperature is {temperature_in_celcius:.2f} celcius, {temperature_in_faranheit:.2f} faranheit, {temperature_in_kelvin:.2f} kelvin")
+print ("hello, world")
+age =  18
+print(age)
