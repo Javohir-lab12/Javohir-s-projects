3de8ef02223656d05a08ebf620abba4c33d31410
Javohir-lab12 <karimovjavohir0612@gmail.com>
2025-10-23 08:38:20 +0500

    Update Ex2.py

--- Ex2.py
+++ Ex2.py
@@ -1,11 +1,11 @@
 def calculate_average(score1, score2, score3):
-    return sum(score1, score2, score3) / 3
+    return sum((score1, score2, score3)) / 3
 
 def drop_lowest(score1, score2, score3):
-    return (sum(score1, score2, score3) - min(score1, score2, score3))/2
+    return (sum((score1, score2, score3)) - min(score1, score2, score3))/2
 
 def calculate_weighted(assignments, midterm, final):
-     return (assignments * .3 + midterm * .3 +final * .4)/3
+     return assignments * 0.3 + midterm * 0.3 + final * 0.4
 
 def determine_grade(average):
     if average >= 90:
@@ -17,26 +17,36 @@ def determine_grade(average):
     elif average >=60:
         return "D"
     else:
-        "F"
+        return "F"
 def needs_improvement(current_avg, target_grade):
     return current_avg >= target_grade 
     
-assignments_score = 
-midterm_score = 
-final_score = 
-score1, score2, score3 = 
-average = 
-print("STUDENT GRADE CALCULATOR")
-print("="*40)
-print(f"Assignment Scores: {score1, score2, score3}")
-print(f"Midterm Score: {midterm_score}")
-print("-"*40)
-print(f"Regular Assignment Average: {calculate_average(score1, score2, score3)}")
-print(f"Average with Lowest Dropped: {drop_lowest(score1, score2, score3)}")
-print(f"Using Better Average:  {drop_lowest(score1, score2, score3)}\n")
-print(f"Weighted Course Average: {calculate_weighted(assignments, midterm_score, final)}")
-print(f"Letter Grade: {determine_grade(average)}\n")
-if average < 90 :
-    print("Needs improvement for an 'A': Yes")
-    print(f"Points needed: 4.05")
-    print(f"Already meets or exceeds 'B' grade requirement")
+def process(assignments_score1, assignments_score2, assignments_score3, midterm_score, final_score):
+    assignment_avg = (assignments_score1 + assignments_score2 + assignments_score3)/3
+    weighted_avg = calculate_weighted(assignment_avg, midterm_score, final_score)
+    print("STUDENT GRADE CALCULATOR")
+    print("="*40)
+    print(f"Assignment Scores: {assignments_score1, assignments_score2, assignments_score3}")
+    print(f"Midterm Score: {midterm_score}")
+    print("-"*40)
+    print(f"Regular Assignment Average: {calculate_average(assignments_score1, assignments_score2, assignments_score3):.2f}")
+    print(f"Average with Lowest Dropped: {drop_lowest(assignments_score1, assignments_score2, assignments_score3):.2f}")
+    print(f"Using Better Average:  {drop_lowest(assignments_score1, assignments_score2, assignments_score3):.2f}\n")
+    print(f"Weighted Course Average: {calculate_weighted(assignment_avg, midterm_score, final_score):.2f}")
+    print(f"Letter Grade: {determine_grade(assignment_avg)}\n")
+    print(f"Needs improvement for an 'A': {"Yes" if needs_improvement(assignment_avg, 90) else "No"}")
+    if assignment_avg < 90:
+        print(f"Points needed: {90 - weighted_avg:.2f}") 
+    else:
+        ...
+    if assignment_avg >= 90:
+        print(f"Already meets or exceeds 'A' grade requirement")
+    elif assignment_avg >= 80:
+        print(f"Already meets or exceeds 'B' grade requirement")
+    elif assignment_avg >= 70:
+        print(f"Already meets or exceeds 'C' grade requirement")
+    elif assignment_avg >= 60:
+        print(f"Already meets or exceeds 'D' grade requirement")
+    else:
+        print(f"Already meets or exceeds 'F' grade requirement")
+process(85, 78, 92, 88, 82)
